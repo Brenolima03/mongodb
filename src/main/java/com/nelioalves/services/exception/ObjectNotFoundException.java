9439e333a7c793034345a91cca59913cd8eaa2bc
Breno Cordeiro <brenocslima@outlook.com>
2023-07-04 12:21:01 -0400

    Obtendo um usuário por id

---
+++ src/main/java/com/nelioalves/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.nelioalves.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
