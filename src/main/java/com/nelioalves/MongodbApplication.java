3aacfae94f2d17654acc381e4a5df243de6ccef3
Breno Cordeiro <brenocslima@outlook.com>
2023-07-04 11:47:56 -0400

    Usando padrão DTO para retornar usuários

--- src/main/java/com/nelioalves/MongodbApplication.java
+++ src/main/java/com/nelioalves/MongodbApplication.java
@@ -9,5 +9,4 @@ public class MongodbApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MongodbApplication.class, args);
 	}
-
 }
--- src/main/java/com/nelioalves/controllers/UserResource.java
+++ src/main/java/com/nelioalves/controllers/UserResource.java
@@ -1,29 +1,29 @@
 package com.nelioalves.controllers;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.domain.User;
+import com.nelioalves.dto.UserDTO;
 import com.nelioalves.services.UserService;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value="/users")
 public class UserResource {
-    @Autowired
-    private UserService service;
-    
-    // @GetMapping
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
-    }
+
+	@Autowired
+	private UserService service;
+	
+	@RequestMapping(method=RequestMethod.GET)
+ 	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
+        List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 }
