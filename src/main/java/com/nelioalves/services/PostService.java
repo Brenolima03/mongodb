2be8b749ebcfec2fb1938924babea5a538703432
Breno Cordeiro <brenocslima@outlook.com>
2023-07-05 19:55:11 -0400

    Consulta simples com query methods

--- src/main/java/com/nelioalves/services/PostService.java
+++ src/main/java/com/nelioalves/services/PostService.java
@@ -7,10 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.domain.Post;
-import com.nelioalves.domain.User;
-import com.nelioalves.dto.UserDTO;
 import com.nelioalves.repository.PostRepository;
-import com.nelioalves.repository.UserRepository;
 import com.nelioalves.services.exception.ObjectNotFoundException;
 
 @Service
@@ -22,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
--- src/main/java/com/nelioalves/services/UserService.java
+++ src/main/java/com/nelioalves/services/UserService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.nelioalves.domain.Post;
 import com.nelioalves.domain.User;
 import com.nelioalves.dto.UserDTO;
 import com.nelioalves.repository.UserRepository;
